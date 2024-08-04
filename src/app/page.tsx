05f5f8d003eac7624cab61116723b114af3c1641
Raazi Muhammed K <raazi6163@gmail.com>
2024-08-04 12:39:26 +0530

    refactor: move topics card

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,45 +1,16 @@
-import Link from "next/link";
-import {
-    Card,
-    CardContent,
-    CardDescription,
-    CardTitle,
-} from "@/components/ui/card";
-
-function TopicCard({
-    title,
-    description,
-    href,
-}: {
-    title: string;
-    description: string;
-    href: string;
-}) {
-    return (
-        <Link href={href}>
-            <Card>
-                <CardContent className="py-4">
-                    <CardTitle className="m-0 mb-2">{title}</CardTitle>
-                    <CardDescription>{description}</CardDescription>
-                </CardContent>
-            </Card>
-        </Link>
-    );
-}
+import TopicCard from "@/components/TopicCard";
 
 export default function Home() {
+    const topics = [
+        { href: "/react", title: "React", description: "Docs for React" },
+        { href: "/graphql", title: "GraphQl", description: "Docs for GraphQL" },
+    ];
+
     return (
         <main className="grid-auto container grid grid-cols-3 gap-8">
-            <TopicCard
-                href="/react"
-                title="React"
-                description="Docs for React"
-            />
-            <TopicCard
-                href="/graphql"
-                title="GraphQl"
-                description="Docs for GraphQL"
-            />
+            {topics.map((topic) => (
+                <TopicCard {...topic} />
+            ))}
         </main>
     );
 }
---
+++ src/components/TopicCard.tsx
@@ -0,0 +1,28 @@
+import Link from "next/link";
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardTitle,
+} from "@/components/ui/card";
+
+export default function TopicCard({
+    title,
+    description,
+    href,
+}: {
+    title: string;
+    description: string;
+    href: string;
+}) {
+    return (
+        <Link href={href}>
+            <Card>
+                <CardContent className="py-4">
+                    <CardTitle className="m-0 mb-2">{title}</CardTitle>
+                    <CardDescription>{description}</CardDescription>
+                </CardContent>
+            </Card>
+        </Link>
+    );
+}
