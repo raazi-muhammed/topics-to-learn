fb058a1d66d3a7467b4bb465ecde15988d4dd8bf
Raazi Muhammed K <raazi6163@gmail.com>
2024-08-04 15:35:52 +0530

    feat: add vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import NavBar from "@/components/layout/NavBar";
 import { ThemeProvider } from "@/providers/theme/theme-provider";
+import { Analytics } from "@vercel/analytics/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -29,6 +30,7 @@ export default function RootLayout({
                     disableTransitionOnChange>
                     <NavBar />
                     {children}
+                    <Analytics />
                 </ThemeProvider>
             </body>
         </html>
