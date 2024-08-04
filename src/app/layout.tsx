b5723c35bdc7aaf467dec31103029ba5c16c9bb5
Raazi Muhammed K <raazi6163@gmail.com>
2024-08-04 13:55:11 +0530

    docs: add favicon and change title

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,6 +18,9 @@ export default function RootLayout({
 }>) {
     return (
         <html lang="en">
+            <head>
+                <link rel="icon" href="/favicon.ico" sizes="any" />
+            </head>
             <body className={inter.className}>
                 <ThemeProvider
                     attribute="class"
--- src/components/layout/NavBar.tsx
+++ src/components/layout/NavBar.tsx
@@ -1,13 +1,21 @@
 import Link from "next/link";
 import React from "react";
 import { ThemeSwitcher } from "./ThemeSwitcher";
+import Image from "next/image";
 
 export default function NavBar() {
     return (
         <header className="sticky top-0 mb-8 bg-accent/20 py-4 backdrop-blur-xl">
             <section className="container flex justify-between align-middle">
-                <Link href="/" className="mt-1">
-                    <h1 className="m-0 font-display text-2xl font-bold text-primary">
+                <Link href="/" className="mt-1 flex gap-1">
+                    <Image
+                        src="/icon.png"
+                        width={12}
+                        height={12}
+                        alt="logo"
+                        className="size-10"
+                    />
+                    <h1 className="m-0 my-auto font-display text-2xl font-bold text-primary">
                         Topics to Learn
                     </h1>
                 </Link>
