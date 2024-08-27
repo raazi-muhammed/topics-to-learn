4f6a704fcf091ccc9889cdbfa797c58a9c712371
rabeeh003 <rabimon003@gmail.com>
2024-08-27 23:10:37 +0530

    add more python topics and structered list, add icons for all topics and change fonts

--- src/components/TopicCard.tsx
+++ src/components/TopicCard.tsx
@@ -5,12 +5,15 @@ import {
     CardDescription,
     CardTitle,
 } from "@/components/ui/card";
+import Image from "next/image";
 
 export default function TopicCard({
     title,
     description,
     href,
+    icon,
 }: {
+    icon: string;
     title: string;
     description: string;
     href: string;
@@ -18,9 +21,12 @@ export default function TopicCard({
     return (
         <Link href={href}>
             <Card>
-                <CardContent className="py-4">
-                    <CardTitle className="m-0 mb-2">{title}</CardTitle>
-                    <CardDescription>{description}</CardDescription>
+                <CardContent className="py-4 flex items-center gap-4">
+                    <Image src={icon} alt="logo" width={50} height={50} className="rounded-sm object-cover" />
+                    <div>
+                        <CardTitle className="m-0 mb-2 font-mono">{title}</CardTitle>
+                        <CardDescription>{description}</CardDescription>
+                    </div>
                 </CardContent>
             </Card>
         </Link>
